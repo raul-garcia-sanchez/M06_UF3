cb6893d62a5d94cbacd258300fb49761f069ed75
raul-garcia-sanchez <raulgarcia200326@gmail.com>
2022-11-23 18:54:45 +0100

    ex3CrearyBorrar finish

--- Ejercicio3CrearyBorrar/ej3diferente.js
+++ Ejercicio3CrearyBorrar/ej3diferente.js
@@ -11,23 +11,43 @@ function cambiarVisualizacion(event){
     let elementoPosterior = document.querySelector("[type='submit']");
 
     if(event.target.value == "otro"){
-        insertarElemento("label", elementoPadre, elementoPosterior, "Especificar:")
-        insertarElemento("input", elementoPadre, elementoPosterior);
+        let atributos = new Map();
+        atributos.set("class","removed");
+        atributos.set("id","removed");
+        insertarElemento("label", elementoPadre, elementoPosterior, "Especificar:", atributos)
+        insertarElemento("input", elementoPadre, elementoPosterior, null, atributos);
     }
     else{
         eliminarElemento();
         
     }
-    
 }
 
 //acabar ejercicio y funcion hecha añadiendo propiedades
-function insertarElemento(tag, elementoPadre, elementoHermano=null, texto=null){
+function insertarElemento(tag, elementoPadre, elementoHermano=null, texto=null, diccionarioAtributos=null){
+    
     let newElement = document.createElement(tag);
     if(texto){
         let textoNodo = document.createTextNode(texto);
         newElement.appendChild(textoNodo);
     }
-
+    if(diccionarioAtributos){
+        for(let key of diccionarioAtributos.keys()){
+            if(key == "class"){
+                newElement.classList.add(diccionarioAtributos.get(key));
+            }
+            else if(key == "id"){
+                newElement.setAttribute(key, diccionarioAtributos.get(key));
+            }
+        }
+    }
     elementoPadre.insertBefore(newElement,elementoHermano);
+}
+
+function eliminarElemento(){
+    let elementosOcultos = document.querySelectorAll("[class='removed']");
+    for(let i = 0; i < elementosOcultos.length; i++){
+        elementosOcultos[i].remove();
+    }
+
 }
