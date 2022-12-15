aeb71b76d6958692c40cdba1aa5ee3584f394112
raul-garcia-sanchez <raul-garcia-sanchez>
2022-12-15 16:58:26 +0100

    modifying

--- JQUERY/Ejercicio6/ejercicio6.js
+++ JQUERY/Ejercicio6/ejercicio6.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
-  $("[name = 'creardorEnlace'").click(function () {
-    $("p").first().append("<a href='http://nuevoenlace.com' class='myClass'>Nuevo Enlace</a><br>");
-  });
 
-  $("[name = 'clonadorEnlace'").click(function () {
-    $("p").last().append($("[class = 'myClass'").first().clone());
-    $("p").last().append("<br>");
-  });
+    $("[name = 'creardorEnlace']").click(function () {
+        $.fn.createLink("http://nuevoenlace.com", "myClass");
+    });
 
+    $("[name = 'clonadorEnlace'").click(function () {
+        $.fn.cloneLink();
+    });
 });
+
+$.fn.createLink = function (href = "", classLInk = null) {
+    if (href != null && classLInk != null) {
+        $("p").first().append("<a href='" + href + "' class='" + classLInk + "'>Nuevo Enlace</a><br>");
+    }
+    else {
+        $("p").first().append("<a href='" + href + "'>Nuevo Enlace</a><br>");
+    }
+
+};
+
+$.fn.cloneLink = function () {
+    $("p").last().append($("[class = 'myClass']").first().clone());
+    $("p").last().append("<br>");
+}
--- JQUERY/Ejercicio7/ejercicio7.js
+++ JQUERY/Ejercicio7/ejercicio7.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    $("[name = 'creardorEnlace'").click(function () {
+    $("[name = 'creardorEnlace']").click(function () {
       $("p").first().append("<a href='http://nuevoenlace.com' class='myClass'>Nuevo Enlace</a><br>");
     });
   
-    $("[name = 'clonadorEnlace'").click(function () {
-      $("p").last().append($("[class = 'myClass'").first().clone());
+    $("[name = 'clonadorEnlace']").click(function () {
+      $("p").last().append($("[class = 'myClass']").first().clone());
       $("p").last().append("<br>");
     });
 
-    $("[name = 'añadirAtributoEnlaceOriginal'").click(function () {
+    $("[name = 'añadirAtributoEnlaceOriginal']").click(function () {
         $("a").first().attr("enlaceOriginal","true");
     })
 
-    $("[name = 'buttonAtributoHref'").click(function () {
-        $("a").first().attr("href",$("[name = 'atributoHref'").val())
+    $("[name = 'buttonAtributoHref']").click(function () {
+        $("a").first().attr("href",$("[name = 'atributoHref']").val())
     })
   
   });
